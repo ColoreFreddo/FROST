196c63d90b6d239eb0bfde5fcaf0f4e46db07d23
colorefreddo <daniel.viola.apple@icloud.com>
2023-05-21 22:56:31 +0200

    new features such as ping and expanded links

--- frost-wosint.py
+++ frost-wosint.py
@@ -1,4 +1,23 @@
 import os
+import pyshorteners
+
+def site_ping_test(URL):
+  response = os.system("curl --head --silent --fail " + site_name + " > /dev/null")
+  if response == 0:
+    return True
+  else:
+    return False
+
+def compressed_dilemma(URL):
+  default_lenght = 20
+  if len(URL) < default_lenght:
+    return False
+  else:
+    return True
+
+def expand_URL(URL):
+  expanded = expand_URL(URL)
+  return expanded 
 
 ascii_art = """
   █████▒██▀███   ▒█████    ██████ ▄▄▄█████▓ █     █░ ▒█████    ██████  ██▓ ███▄    █ ▄▄▄█████▓
@@ -13,4 +32,19 @@ ascii_art = """
 """
 print(ascii_art)
 
-input
+site_name = input("Inesert the URL/IP to analize: ")
+response = site_ping_test(site_name)
+if response:
+  print(f"Site {site_name} is reachable!")
+  if compressed_dilemma(site_name):
+    print("The URL is compressed.")
+    print("Expanding...")
+    expanded_site = expand_URL(site_name)
+    if site_ping_test(expanded_site):
+      print("Expanded correctly!")
+    else:
+      print("Could't expand! (Or maybe is not compressed...)")
+  else:
+    print("The URL is not compressed.")
+else:
+  print(f"Site {site_name} is unreachable! (maybe you misstyped the URL)")
